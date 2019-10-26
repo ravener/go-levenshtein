43b444a65a4bfce6f262ac415e41e32b798c283d
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2019-10-26 10:59:46 +0430

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Go Levenshtein
+[The Levenshtein Edit Distance](https://en.wikipdia.org/wiki/Levenshtein_distance) algorithm in [Golang](https://golang.org)
+
+This is a straight port of [fast-levenshtein](https://github.com/hiddentao/fast-levenshtein) ([npm](https://npmjs.com/package/fast-levenshtein)) from JavaScript.
+
+- As efficient as the original by not storing the whole matrix.
+- Built UTF-8 aware
+- Small, the go file is about 1 KB
+- Easy to use, exposes only one function.
+
+## Install
+```sh
+$ go get github.com/pollen5/go-levenshtein
+```
+
+## Usage
+```go
+package main
+
+import (
+  "fmt"
+  "github.com/pollen5/levenshtein"
+)
+
+func main() {
+  fmt.Println(levenshtein.Get("kitten", "sitting")) // => 3
+
+  // UTF-8 aware
+  fmt.Println(levenshtein.Get("Françe", "France")) // => 1
+}
+```
+
+## License
+[MIT](LICENSE)
---
+++ levenshtein.go
@@ -0,0 +1,75 @@
+package levenshtein
+
+import (
+  "unicode/utf8"
+)
+
+func Get(str1 string, str2 string) int {
+  str1Len := utf8.RuneCountInString(str1)
+  str2Len := utf8.RuneCountInString(str2)
+
+  // Base cases
+  if str1Len == 0 {
+    return str2Len
+  }
+
+  if str2Len == 0 {
+    return str1Len
+  }
+
+  if str1 == str2 {
+    return 0
+  }
+
+  // Two rows
+  var curCol int
+  var nextCol int
+  var i int
+  var j int
+  var tmp int
+  prevRow := make([]int, str2Len + 1)
+  str2Char := make([]rune, str2Len + 1)
+  runes1 := []rune(str1)
+  runes2 := []rune(str2)
+
+  // initialise previous row
+  for i = 0; i < str2Len; i++ {
+    prevRow[i] = i
+    str2Char[i] = runes2[i]
+  }
+  prevRow[str2Len] = str2Len
+
+  var strCmp bool
+  for i = 0; i < str1Len; i++ {
+    nextCol = i + 1
+    for j = 0; j < str2Len; j++ {
+      curCol = nextCol
+
+      // substution
+      strCmp = runes1[i] == str2Char[j]
+
+      nextCol = prevRow[j]
+      // Go doesn't have ternaries :(
+      if !strCmp { nextCol++ }
+
+      // insertion
+      tmp = curCol + 1
+      if nextCol > tmp {
+        nextCol = tmp
+      }
+
+      // deletion
+      tmp = prevRow[j + 1] + 1
+      if(nextCol > tmp) {
+        nextCol = tmp
+      }
+
+      // copy current col value into previous (in preparation for next iteration)
+      prevRow[j] = curCol
+    }
+
+    // copy last col value into previous (in preparation for next iteration)
+    prevRow[j] = nextCol
+  }
+  return nextCol
+}
